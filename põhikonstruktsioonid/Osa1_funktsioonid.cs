1e3854fa0ee67b174129cf0a8f2c3750db9903b4
opilane <opilane@HP-CZC2349HT4>
2025-09-04 13:16:53 +0300

    Add project files.

---
+++ põhikonstruktsioonid/Osa1_funktsioonid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace põhikonstruktsioonid
+{
+    internal class Osa1_funktsioonid
+    {
+        public static float Kalkulaator(float arv1, float arv2)
+        {
+            float k=arv1 * arv2;
+            return k;
+        }
+    }
+}
---
+++ põhikonstruktsioonid/Startclass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace põhikonstruktsioonid
+{
+    internal class Startclass
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.BackgroundColor = ConsoleColor.Red; 
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Tere tulemast! Mis on sinu nimi?"); // Выводит текст на экран
+            string tekst =Console.ReadLine(); //Запрашивает ввод текста
+            Console.WriteLine($"{tekst}, Rõõm näha"); //Выводит то что указал пользователь $=f(Python)
+            int a = 1000;
+            char taht = 'A';
+            Console.Write($"Esimene arv on {a}, Sisesta b=...");
+            int b=int.Parse(Console.ReadLine());
+            Console.WriteLine("Esimene arv on {0}, Sisesta {1}. Summa on {2} ", a, b, a+b);
+            Console.WriteLine("Ujukomaarv");
+            double d = double.Parse(Console.ReadLine());
+            Console.WriteLine(d);
+            float f = float.Parse(Console.ReadLine());
+            Console.WriteLine(f);
+            bool t = true;
+
+            Random rnd =new Random();
+            a = rnd.Next(-100, 200);
+            Console.WriteLine(a);
+            float vastus=Osa1_funktsioonid.Kalkulaator(f, a);
+            Console.WriteLine($"Kalkulaator tulemus: {vastus}");
+        }
+    }
+}
