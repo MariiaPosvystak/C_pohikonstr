73c0cd17fd2c8d2d109d1f316146e98245275ef5
opilane <opilane@HP-CZC2349HT4>
2025-09-05 11:38:54 +0300

    hgf

--- põhikonstruktsioonid/Osa1_funktsioonid.cs
+++ põhikonstruktsioonid/Osa1_funktsioonid.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +14,115 @@ namespace põhikonstruktsioonid
             float k=arv1 * arv2;
             return k;
         }
+        public static string Kuu_nimetus(int kuu_nr)
+        {
+            string kuu = "";
+            switch (kuu_nr)
+            {
+                case 1: kuu = "Jaanuar";break;
+                case 2: kuu = "Veebruar"; break;
+                case 3: kuu = "Märts"; break;
+                case 4: kuu = "Aprill"; break;
+                case 5: kuu = "Mai"; break;
+                case 6: kuu = "Juuni"; break;
+                case 7: kuu = "Juuli"; break;
+                case 8: kuu = "August"; break;
+                case 9: kuu = "September"; break;
+                case 10: kuu = "Oktoober"; break;
+                case 11: kuu = "November"; break;
+                case 12: kuu = "Detsember"; break;
+                default:
+                    kuu = "Vale vähendus!";
+                    break;
+            }
+            return kuu;
+        }
+        public static string Hooaeg(int kuu_nr)
+        {
+            string hoo = "";
+            if (kuu_nr == 1 || kuu_nr == 2 || kuu_nr == 12) //1,2,12
+            {
+                hoo = "Talv";
+            }
+            else if (kuu_nr > 2 && kuu_nr < 6) //3,4,5
+            {
+                hoo = "Kevad";
+            }
+            else if (kuu_nr > 5 && kuu_nr < 9) //6,7,8
+            {
+                hoo = "Suvi";
+            }
+            else if (kuu_nr > 8 && kuu_nr < 12) //9,10,11
+            {
+                hoo = "Sügis";
+            }
+            else
+            {
+                hoo = "???";
+            }
+                return hoo;
+        }
+
+        //1. Ülesanne
+        public static string Pilet(int vanus)
+        {
+            string pilet = "";
+            if (vanus < 6)
+            {
+                pilet = "Tasuta";
+            }
+            else if (vanus > 5 && vanus < 15)
+            {
+                pilet = "Lastepilet";
+            }
+            else if (vanus > 14 && vanus < 66)
+            {
+                pilet = "Täispilet";
+            }
+            else if (vanus > 65)
+            {
+                pilet = "Sooduspilet";
+            }
+            else if (vanus < 0 && vanus < 100)
+            {
+                pilet = "Viga andmetega";
+            }
+            else
+            {
+                pilet = "Erorr";
+            }
+            return pilet;
+        }
+
+        //2.
+        //Спроси имена двух человек и сообщи, что они сегодня сидят рядом в парте.
+
+
+        //3.
+        //Спроси длины стен прямоугольной комнаты и вычисли площадь пола.
+        //Спроси у пользователя, хочет ли он делать ремонт, если ответ положительный,
+        //спроси, сколько стоит квадратный метр, и вычисли стоимость замены пола.
+        public static (float S, float E) Põrand(float pikk, float lai, float metr)
+        {
+            float S = pikk * lai;
+            float E = S * metr;
+            return (S, E);
+        }
+
+        //4.
+        //Найди исходную цену по цене с 30 % скидкой.
+
+        //5.
+        //Спроси температуру и сообщи, превышает ли она восемнадцать градусов(рекомендуемая комнатная температура зимой).
+
+        //6.
+        //Спроси рост человека и сообщи, он низкий, средний или высокий(пределы установи сам).
+
+        //7.
+        //Спроси рост и пол человека и сообщи, он низкий, средний или высокий(может быть несколько вложенных условий).
+
+        //8.
+        //Спроси у человека, хочет ли он купить молоко, булочку, хлеб в магазине.Подсчитай стоимость и сообщи, сколько стоит весь купленный товар.
+
     }
 }
--- põhikonstruktsioonid/Startclass.cs
+++ põhikonstruktsioonid/Startclass.cs
@@ -10,31 +10,150 @@ namespace põhikonstruktsioonid
     {
         public static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
+            Random rnd = new Random();
+            //2.Osa C# Valikute konstruktsionid. Teoria+Ülesanded
 
-            Console.BackgroundColor = ConsoleColor.Red; 
-            Console.ForegroundColor = ConsoleColor.Black;
+            //int kuu_number = rnd.Next(1, 12);
+            //string nimetus = Osa1_funktsioonid.Kuu_nimetus(kuu_number);
+            //Console.WriteLine($"Nr: {kuu_number}.{nimetus}");
 
-            Console.WriteLine("Tere tulemast! Mis on sinu nimi?"); // Выводит текст на экран
-            string tekst =Console.ReadLine(); //Запрашивает ввод текста
-            Console.WriteLine($"{tekst}, Rõõm näha"); //Выводит то что указал пользователь $=f(Python)
-            int a = 1000;
-            char taht = 'A';
-            Console.Write($"Esimene arv on {a}, Sisesta b=...");
-            int b=int.Parse(Console.ReadLine());
-            Console.WriteLine("Esimene arv on {0}, Sisesta {1}. Summa on {2} ", a, b, a+b);
-            Console.WriteLine("Ujukomaarv");
-            double d = double.Parse(Console.ReadLine());
-            Console.WriteLine(d);
-            float f = float.Parse(Console.ReadLine());
-            Console.WriteLine(f);
-            bool t = true;
+            //Console.WriteLine("Kas tahad dekodeerida arv -> nimetusse");
+            //string vastus=Console.ReadLine();
+            //if (vastus.ToLower() != "jah")
+            //{
+            //    Console.WriteLine("Ei taha, siis ei taha");
+            //}
+            //else
+            //{
+            //    try
+            //    {
+            //        Console.Write("Nr: ");
+            //        kuu_number = int.Parse(Console.ReadLine());
+            //        Console.WriteLine(Osa1_funktsioonid.Hooaeg(kuu_number));
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.WriteLine(e);
+            //    }
+            //}
 
-            Random rnd =new Random();
-            a = rnd.Next(-100, 200);
-            Console.WriteLine(a);
-            float vastus=Osa1_funktsioonid.Kalkulaator(f, a);
-            Console.WriteLine($"Kalkulaator tulemus: {vastus}");
+            //1. Ülesanne
+            //Console.Write("Mis su nimi on? ");
+            //string vastus = Console.ReadLine();
+            //if (vastus.ToLower() !="juku")
+            //{
+            //    Console.WriteLine("Me ei lähe kinno");
+            //}
+            //else
+            //{
+            //    try
+            //    {
+            //        Console.Write("Kui vana sa oled?: ");
+            //        int vanus = int.Parse(Console.ReadLine());
+            //        string pilet = Osa1_funktsioonid.Pilet(vanus);
+            //        Console.WriteLine($"Juku su pilet on {pilet}");
+            //    }
+            //    catch (Exception e)
+            //    { 
+            //        Console.WriteLine(e);
+            //    }
+            //}
+
+            //2. Ülesanne !!!!!!!
+            //Спроси имена двух человек и сообщи, что они сегодня сидят рядом в парте.
+            //Console.Write("Mis su nimi on? ");
+            //string nimi1 = Console.ReadLine();
+            //Console.Write("Mis su nimi on? ");
+            //string nimi2 = Console.ReadLine();
+            //if ()
+            //else
+            //{
+            //    try
+            //    {
+            //        Console.Write("Kui vana sa oled?: ");
+            //        int vanus = int.Parse(Console.ReadLine());
+            //        string pilet = Osa1_funktsioonid.Pilet(vanus);
+            //        Console.WriteLine($"Juku su pilet on {pilet}");
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.WriteLine(e);
+            //    }
+            //}
+
+            //3.
+            //Спроси длины стен прямоугольной комнаты и вычисли площадь пола.Спроси у пользователя,
+            //хочет ли он делать ремонт, если ответ положительный, спроси, сколько стоит квадратный метр, и вычисли стоимость замены пола.
+            try
+            {
+                Console.Write("Kui pikk on su põrand? ");
+                float pikk = float.Parse(Console.ReadLine());
+                Console.Write("Kui lai on su põrand? ");
+                float lai = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Kas sa tahad remonti teha? ");
+                string vastus = Console.ReadLine();
+                if (vastus.ToLower() != "jah")
+                {
+                    string S = (Osa1_funktsioonid.Põrand(pikk, lai));
+                    Console.WriteLine($"Su seina {pikk}x{lai} pindala = {S}");
+                    Console.WriteLine("Siis remonti ei tule.");
+                }
+                else
+                {
+                    Console.WriteLine("Kui palju maksab ruutmeeter?");
+                    float metr = float.Parse(Console.ReadLine());
+                    Console.WriteLine($"Su seina {pikk}x{lai} pindala = {S}");
+                    float E = (Osa1_funktsioonid.Põrand(metr, S));
+                    Console.WriteLine(E);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+           
+            //4.
+            //Найди исходную цену по цене с 30 % скидкой.
+
+            //5.
+            //Спроси температуру и сообщи, превышает ли она восемнадцать градусов(рекомендуемая комнатная температура зимой).
+
+            //6.
+            //Спроси рост человека и сообщи, он низкий, средний или высокий(пределы установи сам).
+
+            //7.
+            //Спроси рост и пол человека и сообщи, он низкий, средний или высокий(может быть несколько вложенных условий).
+
+            //8.
+            //Спроси у человека, хочет ли он купить молоко, булочку, хлеб в магазине.Подсчитай стоимость и сообщи, сколько стоит весь купленный товар.
+
+            //1.Osa C# Põhikonstruktsionid. Projekti loomine, ikooni lisamine ja arvutamine. Teoria+Praaktika
+
+            //Console.OutputEncoding = Encoding.UTF8;
+
+            //Console.BackgroundColor = ConsoleColor.Red; 
+            //Console.ForegroundColor = ConsoleColor.Black;
+
+            //Console.WriteLine("Tere tulemast! Mis on sinu nimi?"); // Выводит текст на экран
+            //string tekst =Console.ReadLine(); //Запрашивает ввод текста
+            //Console.WriteLine($"{tekst}, Rõõm näha"); //Выводит то что указал пользователь $=f(Python)
+            //int a = 1000;
+            //char taht = 'A';
+            //Console.Write($"Esimene arv on {a}, Sisesta b=...");
+            //int b=int.Parse(Console.ReadLine());
+            //Console.WriteLine("Esimene arv on {0}, Sisesta {1}. Summa on {2} ", a, b, a+b);
+            //Console.WriteLine("Ujukomaarv");
+            //double d = double.Parse(Console.ReadLine());
+            //Console.WriteLine(d);
+            //float f = float.Parse(Console.ReadLine());
+            //Console.WriteLine(f);
+            //bool t = true;
+
+            //a = rnd.Next(-100, 200);
+            //Console.WriteLine(a);
+            //float vastus=Osa1_funktsioonid.Kalkulaator(f, a);
+            //Console.WriteLine($"Kalkulaator tulemus: {vastus}");
         }
     }
 }
